84dce3ffe145b99fe7dfdb37a88bd30087a33502
Carles Gallardo <carles.gallardo00@gmail.com>
2025-03-13 22:25:31 +0100

    authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,31 @@
-export default function Home() {
-  return <h1>Hello Next.js!</h1>
+import SignoutButton from "@/components/signout";
+import { auth } from "@/lib/auth";
+import { signIn, signUp } from "@/server/users";
+import { headers } from "next/headers";
+
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  return (
+    <main className="flex flex-col gap-3 items-center justify-center p-10">
+      <div className="flex gap-3">
+        <button
+          className="bg-neutral-700 text-white p-2 rounded-md"
+          onClick={signIn}
+        >
+          Sign In
+        </button>
+        <button
+          className="bg-neutral-700 text-white p-2 rounded-md"
+          onClick={signUp}
+        >
+          Sign Up
+        </button>
+        <SignoutButton/>
+      </div>
+      <p>{!session ? "Not authenticated" : session.user.name}</p>
+    </main>
+  )
 }
--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
- 
+import { getSessionCookie } from "better-auth/cookies";
+
 export async function middleware(request: NextRequest) {
-    const session = await auth.api.getSession({
-        headers: await headers()
-    })
- 
-    if(!session) {
-        return NextResponse.redirect(new URL("/sign-in", request.url));
-    }
- 
-    return NextResponse.next();
+  const sessionCookie = getSessionCookie(request);
+
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  return NextResponse.next();
 }
- 
+
 export const config = {
-  runtime: "nodejs",
   matcher: ["/dashboard"], // Apply middleware to specific routes
 };
